896588f49ba8409d1df70e763eaa6562d2e78a0d
Tatyana Strelchenko <Tatyana.Strelchenko@dataart.com>
2021-10-08 17:35:02 +0300

    add setting for style building

---
+++ src/scss-to-less.js
@@ -0,0 +1,32 @@
+const fs = require("fs/promises");
+const { join } = require("path");
+
+async function readScssFile() {
+  try {
+    const file = await fs.readFile(join(__dirname, "antd.customize.scss"));
+
+    const content = file.toString();
+    return content.replace(/\$/g, "@");
+  } catch (err) {
+    console.log("Error", err);
+  }
+}
+
+async function writeLessFile(data) {
+  try {
+    await fs.writeFile(join(__dirname, "antd.customize.less"), data);
+  } catch (err) {
+    console.log("Error", err);
+  }
+}
+
+async function execute() {
+  try {
+    const data = await readScssFile();
+    await writeLessFile(data);
+  } catch (e) {
+    console.log("Error", e);
+  }
+}
+
+execute();
