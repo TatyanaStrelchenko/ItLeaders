8617341e46e46f96f9f9866bc9f35aae37981750
Tatyana Strelchenko <Tatyana.Strelchenko@dataart.com>
2021-11-15 17:10:18 +0200

    fix icons

--- src/components/Sponsors/Sponsors.tsx
+++ src/components/Sponsors/Sponsors.tsx
@@ -1,12 +1,13 @@
 import { Col, Row } from 'antd';
 import Avatar from 'antd/lib/avatar';
-import Icon from '@ant-design/icons';
 
 import graphql from 'babel-plugin-relay/macro';
 import React from 'react';
 import { useLazyLoadQuery } from 'react-relay/hooks';
 
 import { SponsorsQuery } from '../../__generated__/SponsorsQuery.graphql';
+import { ReactComponent as LocationImage } from '../../ assets/images/location.svg';
+import { ReactComponent as MemberImage } from '../../ assets/images/member.svg';
 
 import styles from './Sponsors.module.scss';
 
@@ -39,12 +40,6 @@ const RepositoryNameQuery = graphql`
   }
 `;
 
-// const preloadedQuery = loadQuery<AppRepositoryNameQuery>(RelayEnvironment, RepositoryNameQuery, {});
-
-// type SponsorsProps = {
-//   preloadedQuery: PreloadedQuery<AppRepositoryNameQuery>;
-// };
-
 const Sponsors = () => {
   const data = useLazyLoadQuery<SponsorsQuery>(RepositoryNameQuery, {});
 
@@ -54,62 +49,6 @@ const Sponsors = () => {
   const list = data.user?.sponsors.edges;
 
   console.log({ list });
-
-  const locationSvg = () => (
-    <svg
-      version="1.1"
-      id="Capa_1"
-      x="0px"
-      y="0px"
-      width="1em"
-      height="1em"
-      fill="currentColor"
-      viewBox="0 0 395.71 395.71">
-      <g>
-        <path
-          d="M197.849,0C122.131,0,60.531,61.609,60.531,137.329c0,72.887,124.591,243.177,129.896,250.388l4.951,6.738
-      c0.579,0.792,1.501,1.255,2.471,1.255c0.985,0,1.901-0.463,2.486-1.255l4.948-6.738c5.308-7.211,129.896-177.501,129.896-250.388
-      C335.179,61.609,273.569,0,197.849,0z M197.849,88.138c27.13,0,49.191,22.062,49.191,49.191c0,27.115-22.062,49.191-49.191,49.191
-      c-27.114,0-49.191-22.076-49.191-49.191C148.658,110.2,170.734,88.138,197.849,88.138z"
-        />
-      </g>
-    </svg>
-  );
-
-  const membersSvg = () => (
-    <svg
-      version="1.1"
-      id="Layer_1"
-      x="0px"
-      y="0px"
-      viewBox="0 0 512 512"
-      width="1em"
-      height="1em"
-      fill="currentColor">
-      <g>
-        <g>
-          <path
-            d="M488.727,108.606H325.818V23.273C325.818,10.42,315.398,0,302.545,0H23.273C10.42,0,0,10.42,0,23.273v465.455
-			C0,501.58,10.42,512,23.273,512h186.182h93.091h186.182C501.58,512,512,501.58,512,488.727V131.879
-			C512,119.026,501.58,108.606,488.727,108.606z M325.815,155.152v46.545h-93.088v-46.545H325.815z M325.815,248.242v46.536
-			l-93.088-0.009v-46.527H325.815z M186.182,131.879v92.971c0,0.04-0.006,0.079-0.006,0.12s0.006,0.079,0.006,0.119v92.829
-			c0,0.04-0.006,0.079-0.006,0.119s0.006,0.079,0.006,0.121v147.296h-46.545v-23.282c0-12.853-10.42-23.273-23.273-23.273
-			s-23.273,10.42-23.273,23.273v23.282H46.545V46.545h232.727v62.061h-69.818C196.602,108.606,186.182,119.026,186.182,131.879z
-			 M465.455,465.455h-93.091v-23.282c0-12.853-10.42-23.273-23.273-23.273c-12.853,0-23.273,10.42-23.273,23.273v23.282h-23.273
-			h-69.818v-124.14l116.075,0.012c0.096,0,0.189,0.014,0.286,0.014c0.096,0,0.189-0.014,0.285-0.014l116.081,0.011V465.455z
-			 M465.455,294.793l-93.094-0.009v-46.541h93.094V294.793z M465.455,201.697h-93.094v-46.545h93.094V201.697z"
-          />
-        </g>
-      </g>
-    </svg>
-  );
-
-  const LocationIcon = (props: { style: React.CSSProperties }) => (
-    <Icon component={locationSvg} {...props} />
-  );
-  const MembersIcon = (props: { style: React.CSSProperties }) => (
-    <Icon component={membersSvg} {...props} />
-  );
 
   const listSponsors = list?.map((item: any) => {
     const organizationsList = item.node.organizations ? item.node.organizations.edges : [];
@@ -133,7 +72,7 @@ const Sponsors = () => {
                 {item.node.location && (
                   <address className={styles.location}>
                     <span className={styles.iconHolder}>
-                      <LocationIcon style={{ fontSize: '12px', color: '#8b949e' }} />
+                      <LocationImage color="#8b949e" fontSize="12px" />
                     </span>
                     {item.node.location}
                   </address>
@@ -143,9 +82,7 @@ const Sponsors = () => {
           </div>
           {organizationsList.length > 0 && (
             <div className={styles.memberInfo}>
-              <span className={styles.iconHolder}>
-                <MembersIcon style={{ fontSize: '12px', color: '#8b949e' }} />
-              </span>
+              <MemberImage color="#8b949e" fontSize="12px" />
               Member of&nbsp;
               {count < 3 ? renderOrgList : renderOrgList.slice(2, count)}
               {count > 1 && <span className={styles.infoText}>&nbsp;and&nbsp;{count} more</span>}
